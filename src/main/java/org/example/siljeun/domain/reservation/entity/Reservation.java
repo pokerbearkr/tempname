4e3b58ef4ba19812a559ab07341536d0ba516884
kmchaejin <chaejin040@gmail.com>
2025-05-16 23:24:32 +0900

    feat : entity 수정

--- src/main/java/org/example/siljeun/domain/reservation/entity/Reservation.java
+++ src/main/java/org/example/siljeun/domain/reservation/entity/Reservation.java
@@ -2,6 +2,7 @@ package org.example.siljeun.domain.reservation.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -10,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -17,11 +19,14 @@ import lombok.NoArgsConstructor;
 import org.example.siljeun.domain.seat.entity.SeatStatus;
 import org.example.siljeun.domain.user.entity.User;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Getter
 @NoArgsConstructor
 @Entity
 @Table(name = "reservation")
+@EntityListeners(AuditingEntityListener.class)
 public class Reservation {
 
   @Id
@@ -33,19 +38,18 @@ public class Reservation {
   private User user;
 
   @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "seat_status_id", nullable = false)
+  @JoinColumn(name = "seat_status_id")
   private SeatStatus seatStatus;
 
-  @Column(nullable = false)
   private int price;
 
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
-  private DeliveryFee deliveryFee;
+  private TicketReceipt ticketReceipt;
 
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
-  private DiscountRate discountRate;
+  private Discount discount;
 
   @Column(nullable = false)
   @Enumerated(EnumType.STRING)
@@ -55,11 +59,11 @@ public class Reservation {
   @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime created_at;
 
-  public enum DeliveryFee {
+  public enum TicketReceipt {
     DELIVERY, PICKUP
   }
 
-  public enum DiscountRate {
+  public enum Discount {
     GENERAL, SEVERELY, MILDLY, NATIONAL_MERIT // 일반, 중증, 경증, 국가유공자
   }
 
