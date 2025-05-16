eee81f2035e4ff97b0bd8c6a16d0844976d4150f
pokerbearkr <ogdongwon@gmail.com>
2025-05-16 09:51:57 +0900

    init

---
+++ src/main/java/org/example/siljeun/SiljeunApplication.java
@@ -0,0 +1,13 @@
+package org.example.siljeun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiljeunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiljeunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/siljeun/SiljeunApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.siljeun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiljeunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
