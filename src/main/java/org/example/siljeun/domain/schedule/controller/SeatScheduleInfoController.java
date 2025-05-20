2d12ffdb5f52132eef2478465b9872dbf2ab5d02
crocusia <132359536+crocusia@users.noreply.github.com>
2025-05-20 16:40:59 +0900

    feat : 회차 생성 시, 회차별 좌석 정보 생성 기능 추가

---
+++ src/main/java/org/example/siljeun/domain/schedule/controller/SeatScheduleInfoController.java
@@ -0,0 +1,13 @@
+package org.example.siljeun.domain.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/schedules")
+public class SeatScheduleInfoController {
+}
--- src/main/java/org/example/siljeun/domain/seat/entity/SeatScheduleInfo.java
+++ src/main/java/org/example/siljeun/domain/seat/entity/SeatScheduleInfo.java
@@ -22,20 +22,32 @@ import org.example.siljeun.global.entity.BaseEntity;
 @Table(name = "seat_schedule_info")
 public class SeatScheduleInfo extends BaseEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "seat_id", nullable = false)
-  private Seat seat;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
 
-  //회차 정보
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "schedule_id", nullable = false)
-  private Schedule schedule;
+    //회차 정보
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
 
-  private SeatStatus status;
-  private String grade;
-  private int price;
+    private SeatStatus status;
+    private String grade;
+    private int price;
+
+    private SeatScheduleInfo(Seat seat, Schedule schedule, SeatStatus status, String grade, int price) {
+        this.seat = seat;
+        this.schedule = schedule;
+        this.status = status;
+        this.grade = grade;
+        this.price = price;
+    }
+
+    public static SeatScheduleInfo from(Seat seat, Schedule schedule, SeatStatus status, String grade, int price) {
+        return new SeatScheduleInfo(seat, schedule, status, grade, price);
+    }
 }
