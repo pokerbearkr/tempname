6eb8c6e387892aa834edc7ffd08966e1e4a9eac1
crocusia <132359536+crocusia@users.noreply.github.com>
2025-05-22 11:54:31 +0900

    feat : Seat 관련 엔티티, 데이터 생성 api, 분산락 기반 좌석 선택 기능 추가 (#15)
    * feat : Seat 엔티티 필드, Status 타입, Dto 추가
    
    * feat : 공연장 좌석 정보 생성을 위한 3-layer 구현
    
    * feat : 회차 생성 시, 회차별 좌석 정보 생성 기능 추가
    
    * fix : Seat의 일부 필드명 DB와 충돌 문제 해결
    
    * feat : Aop를 활용한 Redisson 분산락 기능 추가
    
    * feat : 좌석 선택 동시성 제어 테스트 코드 추가

---
+++ src/main/java/org/example/siljeun/domain/venue/controller/VenueSeatController.java
@@ -0,0 +1,32 @@
+package org.example.siljeun.domain.venue.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.siljeun.domain.seat.dto.request.SeatCreateRequest;
+import org.example.siljeun.domain.venue.service.VenueSeatService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/venues")
+public class VenueSeatController {
+
+    private final VenueSeatService venueSeatService;
+
+    //좌석 정보를 CSV 파일 또는 GUI로 다수의 정보를 한번에 등록한다.
+    @PostMapping("/{venueId}/seats")
+    public ResponseEntity<Void> createVenueSeats(
+            @PathVariable Long venueId,
+            @RequestBody @Valid List<SeatCreateRequest> seatCreateRequests
+    ){
+        venueSeatService.createSeats(venueId, seatCreateRequests);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/org/example/siljeun/global/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package org.example.siljeun.global.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String host;
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    private static final String REDISSON_PREFIX = "redis://";
+
+    //for redisson
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress(REDISSON_PREFIX + host + ":" + port);
+                //.setPassword(null); //현재 비밀번호가 별도로 설정되어있지 않기 때문에 null
+        return Redisson.create(config);
+    }
+
+    //for redis
+    @Bean
+    public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Long> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<>(Long.class)); // Long 값 직렬화
+        return redisTemplate;
+    }
+}
